326bd3475b1f88d4db73868b3fd874b37fc71406
Paul Sinnett <paul.sinnett@gmail.com>
2019-08-24 09:03:36 +0100

    Initial commit

---
+++ Assets/AsynchronousLevelLoad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AsynchronousLevelLoad : MonoBehaviour
+{
+	public Log log;
+
+	void Start()
+	{
+		log.AddLine("Start loading level");
+		StartCoroutine(LoadLevel("TestLevel"));
+	}
+
+	IEnumerator LoadLevel(string level)
+	{
+		Stopwatch stopwatch = Stopwatch.StartNew();
+		AsyncOperation loader = SceneManager.LoadSceneAsync(level);
+		loader.allowSceneActivation = false;
+		while (loader.progress < 0.9f)
+		{
+			stopwatch.Start();
+			yield return null;
+			stopwatch.Stop();
+			log.AddLine(string.Format("Loading frame took {0} seconds", stopwatch.Elapsed));
+			stopwatch.Reset();
+		}
+		log.AddLine("Level load complete");
+	}
+}
---
+++ Assets/Log.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour {
+
+	public int maxLines = 24;
+	Text log;
+	Queue<string> lines = new Queue<string>();
+
+	void Awake () {
+		log = GetComponent<Text>();
+		for (int i = 0; i < maxLines; ++i)
+		{
+			lines.Enqueue("");
+		}
+	}
+
+	public void AddLine(string line)
+	{
+		lines.Dequeue();
+		lines.Enqueue(line);
+		StringBuilder logBuilder = new StringBuilder();
+		foreach (string row in lines)
+		{
+			logBuilder.AppendLine(row);
+		}
+		log.text = logBuilder.ToString();
+	}
+}
